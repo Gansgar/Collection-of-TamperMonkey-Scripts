e6f18e6d18c72eb6f2c59bca4f346d66b89c6c03
Georg Friedrich <georg.a.friedrich@gmail.com>
2020-04-18 15:33:00 +0200

    Update Hide Play icon in player on RWTH moodle.js
    Added description

--- Hide Play icon in player on RWTH moodle.js
+++ Hide Play icon in player on RWTH moodle.js
@@ -2,7 +2,7 @@
 // @name         Hide Play icon in player on RWTH moodle
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
+// @description  Removes the play icon from RWTH-Moodle videos as they are in the way, when you pause the video.
 // @author       You
 // @match        *://engage.streaming.rwth-aachen.de/*
 // @grant        GM_addStyle
